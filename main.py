23578e409e4f1cab2bbd4a577916ccd266ed796e
aniejek <nowywisniowy@gmail.com>
2017-11-13 11:11:57 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,65 @@
+class Entity():
+    def __init__(self):
+        self.args = None
+        self.primary_key = 0
+
+
+class Dyspozytornie(Entity):
+    def __init__(self, miasto):
+        self.args = [('Miasto', miasto)]
+        self.primary_key = 0
+
+
+class Dyspozytorzy(Entity):
+    def __init__(self, imie, nazwisko, pesel, fk_dyspozytornie):
+        self.args = [('Imie', imie), ('Nazwisko', nazwisko), ('PESEL', pesel),
+                     ('FK_Dyspozytornie', fk_dyspozytornie)]
+        self.primary_key = 2
+
+
+class Kierowcy(Entity):
+    def __init__(self, imie, nazwisko, pesel, data_rejestracji):
+        self.args = [('Imie', imie), ('Nazwisko', nazwisko), ('PESEL', pesel),
+                     ('Data Rejestracji', data_rejestracji)]
+        self.primary_key = 2
+
+
+class Klienci(Entity):
+    def __init__(self, imie, nazwisko, telefon, id):
+        self.args = [('Imie', imie), ('Nazwisko', nazwisko), ('Telefon', telefon),
+                     ('Id', id)]
+        self.primary_key = 0 #TODO
+
+
+class Przejazdy(Entity):
+    def __init__(self, data, id, ocena, poczatek_ulica, poczatek_numer_domu,
+                 poczatek_miasto, koniec_ulica, koniec_numer_domu, koniec_miasto,
+                 koszt, napiwek, czas, fk_klienci, fk_kierowcy, fk_dyspozytorzy):
+        self.args = [('Data', data), ('Id', id), ('Ocena', ocena),
+                     ('Początek_ulica', poczatek_ulica),
+                     ('Poczatek_numer_domu', poczatek_numer_domu),
+                     ('Poczatek_miasto', poczatek_miasto),
+                     ('Koniec_ulica', koniec_ulica),
+                     ('Koniec_numer_domu', koniec_numer_domu),
+                     ('Koniec_miasto', koniec_miasto), ('koszt', koszt),
+                     ('Napiwek', napiwek), ('Czas', czas),
+                     ('FK_Klienci', fk_klienci), ('FK_Kierowcy', fk_kierowcy),
+                     ('FK_Dyspozytorzy', fk_dyspozytorzy)]
+        self.primary_key = 1
+
+
+class Aplikacja(Entity):
+    def __init__(self, ulica, numer_domu, miasto, zrodlo, data_rejestracji,
+                 fk_klienci):
+        self.args = [('Ulica', ulica), ('Numer_domu', numer_domu),
+                     ('Miasto', miasto), ('Skad_sie_dowiedzial', zrodlo),
+                     ('Data_rejestracji', data_rejestracji),
+                     ('FK_Klienci', fk_klienci)]
+        self.primary_key = 5
+
+
+class Karta(Entity):
+    def __init__(self, numer, kod, fk_klient):
+        self.args = [('Numer', numer), ('Kod', kod), ('FK_Klient', fk_klient)]
+        self.primary_key = 0
+pass
