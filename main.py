b1b7845a3f2ed7b27435cb9cc61e557fc20eef68
aniejek <nowywisniowy@gmail.com>
2017-11-20 18:34:33 +0100

    add function which generates pesels

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ LAST_NAME_LIST_SIZE = t.size_of_list(LAST_NAME_LIST)
 
 LICZBA_DYSPOZYTOROW = int(input('Podaj liczbę dyspozytorow: '))
 LICZBA_KIEROWCOW = int(input('Prodaj liczbe kierowcow: '))
-#LICZBA_KLIENTOW = int(input('Podaj liczbe klientow: '))
-#LICZBA_SAMOCHODOW = int(input('Podaj liczbe samochodow: '))
-#LICZBA_APLIKACJI = int(input('Podaj liczbe aplikacji: '))
-#LICZBA_KART = int(input('Podaj liczbe kart platniczych: '))
-#LICZBA_WYKORZYSTAN = int(input('Podaj liczbe wykorzystan: '))
-#LICZBA_PRZEJAZDOW = int(input('Podaj liczbe przejazdow: '))
+LICZBA_KLIENTOW = int(input('Podaj liczbe klientow: '))
+LICZBA_SAMOCHODOW = int(input('Podaj liczbe samochodow: '))
+LICZBA_APLIKACJI = int(input('Podaj liczbe aplikacji: '))
+LICZBA_KART = int(input('Podaj liczbe kart platniczych: '))
+LICZBA_WYKORZYSTAN = int(input('Podaj liczbe wykorzystan: '))
+LICZBA_PRZEJAZDOW = int(input('Podaj liczbe przejazdow: '))
 
 dyspozytornie = [t.Dyspozytornie('Gdańsk'), t.Dyspozytornie('Wrocław'), t.Dyspozytornie('Poznań')]
 dyspozytorzy = []
